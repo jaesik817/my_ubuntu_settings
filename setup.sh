dfe6e7340e71bbe814ffa405b53d1e393ff20a9f
Jaesik Yoon <jaesik817@gmail.com>
2019-07-12 12:33:56 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get update
+apt update
 
 cp bashrc ~/.bashrc
 cp tmux.reset.conf ~/.tmux.reset.conf
@@ -11,11 +11,11 @@ cp zshrc ~/.zshrc
 cp -R vim ~/.vim
 
 # Install tmux
-sudo apt-get -y install tmux
+apt -y install tmux
 
 # Install zsh
-sudo apt-get -y install zsh
-sudo apt-get -y install git-core
+apt -y install zsh
+apt -y install git-core
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 rm -rf ~/.zshrc
 cp zshrc ~/.zshrc
